3d73e7fb6e91e33f169950f1e9ee1e7a0d2d3fd1
James R <jarichards123@gmail.com>
2024-04-08 16:57:27 +0100

    Vue Master lesson 5

--- main.js
+++ main.js
@@ -4,7 +4,14 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: [
+                {id: 1, size: 'XL'}, 
+                {id: 2, size: 'Large'}, 
+                {id: 3, size: 'Medium'},
+                {id: 4, size: 'Small'},
+                {id: 5, size: 'XS'}
+                ]
         }
     }
 })
